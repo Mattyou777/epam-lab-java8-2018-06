8fd21346c10b05195c407ae35ddbc15eaf0af4b9
Matvei <mattyou777@gmail.com>
2018-07-09 00:34:37 +0300

    Lambda part 3 Exercise 4

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,5 +1,8 @@
 package lambda.part3.exercise;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.Consumer;
 import lambda.data.Employee;
 import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
@@ -16,90 +19,132 @@ import static org.junit.Assert.assertEquals;
 @SuppressWarnings({"unused", "ConstantConditions"})
 public class Exercise4 {
 
-    private static class LazyCollectionHelper<T, R> {
+  private static class LazyCollectionHelper<T, R> {
 
-        public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
-        }
+    private List<T> source;
+    private Queue<R> queue;
+    private Consumer<T> consumer;
 
-        public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
-        }
-
-        public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
-            throw new UnsupportedOperationException();
-        }
-
-        public List<R> force() {
-            throw new UnsupportedOperationException();
-        }
+    private LazyCollectionHelper(List<T> source, Queue<R> queue, Consumer<T> consumer) {
+      this.source = source;
+      this.queue = queue;
+      this.consumer = consumer;
     }
 
-    @Test
-    public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
-        List<Employee> employees = getEmployees();
-
-        List<Integer> codes = null;
-        // TODO              LazyCollectionHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .force();
-        assertEquals(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"), codes);
+    public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
+      Queue<T> nextQueue = new LinkedList<>();
+      return new LazyCollectionHelper<>(list, nextQueue, nextQueue::offer);
     }
 
-    private static List<Integer> calcCodes(String...strings) {
-        List<Integer> codes = new ArrayList<>();
-        for (String string : strings) {
-            for (char letter : string.toCharArray()) {
-                codes.add((int) letter);
-            }
+    public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
+      Queue<U> nextQueue = new LinkedList<>();
+      return new LazyCollectionHelper<>(source, nextQueue, consumer.andThen(c -> {
+        while (!queue.isEmpty()) {
+          flatMapping.apply(queue.poll())
+              .forEach(nextQueue::offer);
         }
-        return codes;
+      }));
     }
 
-    private static List<Employee> getEmployees() {
-        return Arrays.asList(
-                new Employee(
-                        new Person("Иван", "Мельников", 30),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Александр", "Дементьев", 28),
-                        Arrays.asList(
-                                new JobHistoryEntry(1, "tester", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Дмитрий", "Осинов", 40),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "QA", "yandex"),
-                                new JobHistoryEntry(1, "QA", "mail.ru"),
-                                new JobHistoryEntry(1, "dev", "mail.ru")
-                        )),
-                new Employee(
-                        new Person("Анна", "Светличная", 21),
-                        Collections.singletonList(
-                                new JobHistoryEntry(1, "tester", "T-Systems")
-                        )),
-                new Employee(
-                        new Person("Игорь", "Толмачёв", 50),
-                        Arrays.asList(
-                                new JobHistoryEntry(5, "tester", "EPAM"),
-                                new JobHistoryEntry(6, "QA", "EPAM")
-                        )),
-                new Employee(
-                        new Person("Иван", "Александров", 33),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "QA", "T-Systems"),
-                                new JobHistoryEntry(3, "QA", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM")
-                        ))
-        );
+    public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
+      Queue<U> nextQueue = new LinkedList<>();
+      return new LazyCollectionHelper<>(source, nextQueue, consumer.andThen(c -> {
+        while (!queue.isEmpty()) {
+          nextQueue.offer(mapping.apply(queue.poll()));
+        }
+      }));
     }
+
+    public List<R> force() {
+      source.forEach(consumer);
+      return new ArrayList<>(queue);
+    }
+  }
+
+  @Test
+  public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
+    List<Employee> employees = getEmployees();
+
+    List<Integer> codes = LazyCollectionHelper.from(employees)
+        .flatMap(Employee::getJobHistory)
+        .map(JobHistoryEntry::getPosition)
+        .flatMap(Exercise4::extractCharacters)
+        .map(Exercise4::extractCodes)
+        .force();
+    //               LazyCollectionHelper.from(employees)
+    //                                   .flatMap(Employee -> JobHistoryEntry)
+    //                                   .map(JobHistoryEntry -> String(position))
+    //                                   .flatMap(String -> Character(letter))
+    //                                   .map(Character -> Integer(code letter)
+    //                                   .force();
+    assertEquals(
+        calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"),
+        codes);
+  }
+
+  private static Integer extractCodes(Character character) {
+    return (int) character;
+  }
+
+  private static List<Character> extractCharacters(String s) {
+    List<Character> characters = new ArrayList<>();
+    for (char character : s.toCharArray()) {
+      characters.add(character);
+    }
+    return characters;
+  }
+
+  private static List<Integer> calcCodes(String... strings) {
+    List<Integer> codes = new ArrayList<>();
+    for (String string : strings) {
+      for (char letter : string.toCharArray()) {
+        codes.add((int) letter);
+      }
+    }
+    return codes;
+  }
+
+  private static List<Employee> getEmployees() {
+    return Arrays.asList(
+        new Employee(
+            new Person("Иван", "Мельников", 30),
+            Arrays.asList(
+                new JobHistoryEntry(2, "dev", "EPAM"),
+                new JobHistoryEntry(1, "dev", "google")
+            )),
+        new Employee(
+            new Person("Александр", "Дементьев", 28),
+            Arrays.asList(
+                new JobHistoryEntry(1, "tester", "EPAM"),
+                new JobHistoryEntry(1, "dev", "EPAM"),
+                new JobHistoryEntry(1, "dev", "google")
+            )),
+        new Employee(
+            new Person("Дмитрий", "Осинов", 40),
+            Arrays.asList(
+                new JobHistoryEntry(3, "QA", "yandex"),
+                new JobHistoryEntry(1, "QA", "mail.ru"),
+                new JobHistoryEntry(1, "dev", "mail.ru")
+            )),
+        new Employee(
+            new Person("Анна", "Светличная", 21),
+            Collections.singletonList(
+                new JobHistoryEntry(1, "tester", "T-Systems")
+            )),
+        new Employee(
+            new Person("Игорь", "Толмачёв", 50),
+            Arrays.asList(
+                new JobHistoryEntry(5, "tester", "EPAM"),
+                new JobHistoryEntry(6, "QA", "EPAM")
+            )),
+        new Employee(
+            new Person("Иван", "Александров", 33),
+            Arrays.asList(
+                new JobHistoryEntry(2, "QA", "T-Systems"),
+                new JobHistoryEntry(3, "QA", "EPAM"),
+                new JobHistoryEntry(1, "dev", "EPAM")
+            ))
+    );
+  }
 
 }
